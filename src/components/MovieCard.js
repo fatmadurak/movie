55a50d45db9daadcba59e22a39fa5198b8c43fcd
fatmadurak <fatmabadillidurak@gmail.com>
2023-11-16 11:39:06 +0300

    added notfoundImage

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import "../assets/css/MovieCard.css"
+import "../assets/css/MovieCard.css";
+import defaultImage from "../assets/images/notfound.png";
 
 function MovieCard({ movie }) {
-  const { Poster, Title,Year,Type,imdbId } = movie;
+  const { Poster, Title, Year, Type, imdbId } = movie;
 
   return (
     <>
-    <div className='Card'>
-    <img className='poster' src={Poster} alt="image" />
-    <div className='info'>
-    <p>Tür:{Type}</p>
-    <p>Yıl:{Year}</p>
-    </div>
-  
-    <p className='title'>{Title}</p>
-    </div>
-     
-  
+      <div className='Card'>
+        {Poster !== "N/A" ? (
+          <>
+            <img className='poster' src={Poster} alt="image" />
+            <div className='info'>
+              <p>Tür:{Type}</p>
+              <p>Yıl:{Year}</p>
+            </div>
+            <p className='title'>{Title}</p>
+          </>
+        ) : (
+          <img className='poster' src={defaultImage} alt="Not Found" />
+        )}
+      </div>
     </>
   );
 }
